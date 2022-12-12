6fe2698557a5ffafa61b7116dcb8fe06fbd4867c
Agustín Ventura <agustin.ventura@mango.com>
2022-12-12 12:21:13 +0100

    Extracts HelloWorld widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Center(child:Text(
-    'Hello World',
-    textDirection: TextDirection.ltr)
-  )
+  runApp(HelloWorld()
   );
+}
+
+class HelloWorld extends StatelessWidget {
+  const HelloWorld({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child:Text(
+      'Hello World',
+      textDirection: TextDirection.ltr)
+    );
+  }
 }
